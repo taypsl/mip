d0491feb0a6dc9d4dc7c9e4dccbd4dcb6ecf8684
Taylor Parsell <tayparsell@gmail.com>
2018-04-12 16:44:58 -0700

    registry links added

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -6,12 +6,14 @@ export default class Registry extends Component {
 		return (
 			<div className="Registry section static">
 				<div className="Registry-links">
-					<div className="Registry-link"><div className="Logo target"></div></div>
-					<div className="Registry-link"><div className="Logo cAndB"></div></div>
-					<div className="Registry-link"><div className="Logo amazon"></div></div>
-					<div className="Registry-link"><div className="Logo giveDirectly"></div></div>
+					<div className="Registry-link"><a href="https://www.target.com/" target="_blank"><div className="Logo target"></div></a></div>
+					<div className="Registry-link"><a href="https://www.crateandbarrel.com/" target="_blank"><div className="Logo cAndB"></div></a></div>
+					<div className="Registry-link"><a href="https://www.amazon.com/" target="_blank"><div className="Logo amazon"></div></a></div>
+					<div className="Registry-link"><a href="https://www.givedirectly.org/" target="_blank"><div className="Logo giveDirectly"></div></a></div>
 				</div>
-				<div className="Charitable"></div>
+				<div className="Charitable-text">
+					<p>Additionally, we ask that you consider making a contribution to <a href="https://www.givedirectly.org/" target="_blank">givedirectly.org</a> in Kip and Marilyn's name. <a href="https://www.givedirectly.org/" target="_blank">GiveDirectly</a> gives money worldwide to people in need.</p>
+				</div>
 			</div>
 		)
 	}
